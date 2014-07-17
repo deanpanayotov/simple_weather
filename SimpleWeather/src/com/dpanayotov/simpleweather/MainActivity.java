04a964ec830c9681b4a47d7d85478068e6aef288
Dean Panayotov <dpanayotov@melontech.com>
2014-07-17 17:58:21 +0300

    simple volley json test;

--- SimpleWeather/src/com/dpanayotov/simpleweather/MainActivity.java
+++ SimpleWeather/src/com/dpanayotov/simpleweather/MainActivity.java
@@ -1,15 +1,22 @@
 package com.dpanayotov.simpleweather;
 
+import org.json.JSONObject;
+
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+
+import com.android.volley.Request.Method;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.dpanayotov.simpleweather.general.SimpleWeatherApplication;
 
 public class MainActivity extends Activity {
 
@@ -60,6 +67,31 @@ public class MainActivity extends Activity {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }
+    }
+    @Override
+    protected void onResume() {
+    	super.onResume();
+    	String tag_json_obj = "json_obj_req";
+    	 
+    	String url = "http://api.androidhive.info/volley/person_object.json";    
+    	         
+    	        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Method.GET,
+    	                url, null,
+    	                new Response.Listener<JSONObject>() {
+    	 
+    	                    @Override
+    	                    public void onResponse(JSONObject response) {
+    	                        Log.d("test", response.toString());
+    	                    }
+    	                }, new Response.ErrorListener() {
+    	 
+    	                    @Override
+    	                    public void onErrorResponse(VolleyError error) {
+    	                    	Log.d("test", "Error: " + error.getMessage());
+    	                    }
+    	                });
+    	 
+    	((SimpleWeatherApplication)getApplication()).addToRequestQueue(jsonObjReq, tag_json_obj);
     }
 
 }
