1c7de17429b98909b74ac579e52c9164f41acb32
Dean Panayotov <dpanayotov@melontech.com>
2015-07-11 14:17:37 +0300

    Added ViewHolder and using ButterKnife in ForecastAdapter.

--- SimpleWeather/app/src/main/java/com/dpanayotov/simpleweather/activity/forecast/fragment/list/ForecastAdapter.java
+++ SimpleWeather/app/src/main/java/com/dpanayotov/simpleweather/activity/forecast/fragment/list/ForecastAdapter.java
@@ -16,12 +16,14 @@ import com.dpanayotov.simpleweather.util.BitmapCirclesUtil;
 import com.dpanayotov.simpleweather.util.DateUtil;
 import com.dpanayotov.simpleweather.util.WeatherImageUtil;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class ForecastAdapter extends ArrayAdapter<Forecast> {
 	private Context mContext;
 	private boolean mIsDaily;
 
-	public ForecastAdapter(Context context, List<Forecast> objects,
-			boolean isDaily) {
+	public ForecastAdapter(Context context, List<Forecast> objects, boolean isDaily) {
 		super(context, R.layout.layout_forecast_item, objects);
 		mContext = context;
 		mIsDaily = isDaily;
@@ -29,59 +31,63 @@ public class ForecastAdapter extends ArrayAdapter<Forecast> {
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		Forecast f = getItem(position);
-		View v = convertView;
-		if (v == null) {
-			LayoutInflater inflater = (LayoutInflater) getContext()
-					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			v = inflater.inflate(R.layout.layout_forecast_item, null);
-		}
-		String formattedTime = DateUtil.getFormatedDate(f.getTime(),
-				mIsDaily ? DateUtil.FORECAST_LIST_FORMAT_DAILY
-						: DateUtil.FORECAST_LIST_FORMAT_HOURLY);
-		v.findViewById(R.id.midnight_bar).setVisibility(
-				!mIsDaily
-						&& DateUtil.FORECAST_LIST_MIDNIGHT
-								.equals(formattedTime) ? View.VISIBLE
-						: View.GONE);
+        Forecast forecast = getItem(position);
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = ((LayoutInflater) getContext().getSystemService(Context
+                    .LAYOUT_INFLATER_SERVICE)).inflate(R.layout.layout_forecast_item, null);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        String formattedTime = DateUtil.getFormatedDate(forecast.getTime(), mIsDaily ?
+                DateUtil.FORECAST_LIST_FORMAT_DAILY : DateUtil.FORECAST_LIST_FORMAT_HOURLY);
+        holder.midnightBar.setVisibility(!mIsDaily && DateUtil.FORECAST_LIST_MIDNIGHT
+                .equals(formattedTime) ? View.VISIBLE : View.GONE);
+        holder.item.setBackgroundColor(mContext.getResources().getColor(position % 2 == 0 ?
+                R.color.terra_terra : R.color.terra_terra_terra));
+        holder.time.setText(formattedTime);
+        if (forecast.getIcon() == null && forecast.getSummary() == null) {
+            holder.data.setVisibility(View.GONE);
+            holder.noData.setVisibility(View.VISIBLE);
+        } else {
+            holder.data.setVisibility(View.VISIBLE);
+            holder.noData.setVisibility(View.GONE);
 
-		v.findViewById(R.id.item).setBackgroundColor(
-				mContext.getResources().getColor(
-						position % 2 == 0 ? R.color.terra_terra
-								: R.color.terra_terra_terra));
-		((TextView) v.findViewById(R.id.time)).setText(formattedTime);
-		if (f.getIcon() == null && f.getSummary() == null) {
-			v.findViewById(R.id.data).setVisibility(View.GONE);
-			v.findViewById(R.id.no_data).setVisibility(View.VISIBLE);
-
-		} else {
-			v.findViewById(R.id.data).setVisibility(View.VISIBLE);
-			v.findViewById(R.id.no_data).setVisibility(View.GONE);
-
-			((ImageView) v.findViewById(R.id.icon))
-					.setImageResource(WeatherImageUtil.returnImageResource(f
-							.getIcon()));
-			((ImageView) v.findViewById(R.id.precip))
-					.setImageBitmap(BitmapCirclesUtil.getPrecipCircle(
-							f.getPrecipIntensity(), f.getPrecipProbability()));
-			float temperature;
-			if (mIsDaily) {
-				temperature = (f.getTemperatureMin() + f.getTemperatureMax()) / 2;
-			} else {
-				temperature = f.getTemperature();
-			}
-			((ImageView) v.findViewById(R.id.temperature))
-					.setImageBitmap(BitmapCirclesUtil
-							.getTemperatureCircle(temperature));
-			((TextView) v.findViewById(R.id.temperature_text)).setText(""
-					+ (int) temperature);
-			((ImageView) v.findViewById(R.id.coluds))
-					.setImageBitmap(BitmapCirclesUtil.getCloudCircle(f
-							.getCloudCover()));
-			((ImageView) v.findViewById(R.id.wind))
-					.setImageBitmap(BitmapCirclesUtil.getWindCircle(f
-							.getWindSpeed()));
-		}
-		return v;
+            holder.icon.setImageResource(WeatherImageUtil.returnImageResource(forecast.getIcon()));
+            holder.precip.setImageBitmap(BitmapCirclesUtil.getPrecipCircle(
+                    forecast.getPrecipIntensity(), forecast.getPrecipProbability()));
+            float temperature;
+            if (mIsDaily) {
+                temperature = (forecast.getTemperatureMin() + forecast.getTemperatureMax()) / 2;
+            } else {
+                temperature = forecast.getTemperature();
+            }
+            holder.temperature.setImageBitmap(BitmapCirclesUtil.getTemperatureCircle(temperature));
+            holder.temperatureText.setText("" + (int) temperature);
+            holder.coluds.setImageBitmap(BitmapCirclesUtil.getCloudCircle(forecast.getCloudCover()));
+            holder.wind.setImageBitmap(BitmapCirclesUtil.getWindCircle(forecast.getWindSpeed()));
+        }
+        return convertView;
 	}
+
+    static class ViewHolder {
+
+        @Bind(R.id.midnight_bar) View midnightBar;
+        @Bind(R.id.item) View item;
+        @Bind(R.id.time) TextView time;
+        @Bind(R.id.data) View data;
+        @Bind(R.id.no_data) View noData;
+        @Bind(R.id.icon) ImageView icon;
+        @Bind(R.id.precip) ImageView precip;
+        @Bind(R.id.temperature) ImageView temperature;
+        @Bind(R.id.temperature_text) TextView temperatureText;
+        @Bind(R.id.coluds) ImageView coluds;
+        @Bind(R.id.wind) ImageView wind;
+
+        public ViewHolder(View view){
+            ButterKnife.bind(this, view);
+        }
+    }
 }
